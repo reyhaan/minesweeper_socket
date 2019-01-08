688051006b9e93c5292a10c5cb4a57a5e7f12318
Tk Yussuff <tktaofik@gmail.com>
2019-01-08 17:25:12 -0500

    Revert "added certify"
    This reverts commit 8d01bddb667b2f6c694df7f82c20574b41a77831.

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import socketio
-import certifi
 from aiohttp.web import Application, RouteTableDef, json_response, Request, run_app
 
 from api.tracking import sio
@@ -11,15 +10,12 @@ load_dotenv()
 app: Application = Application()
 routes: RouteTableDef = RouteTableDef()
 
-
-@routes.get("/ping")
+@routes.get('/ping')
 async def ping(request: Request):
-    return json_response({"ping": "pong"})
+  return json_response({'ping': 'pong'})
 
-
-if __name__ == "__main__":
-    app.add_routes(routes)
-    sio.attach(app)
-    certifi.where()
-    run_app(app, port=os.getenv("PORT", 3000))
-
+if __name__ == '__main__':
+  app.add_routes(routes)
+  sio.attach(app)
+  run_app(app, port=os.getenv('PORT', 3000))
+  
--- example/admin1.py
+++ example/admin1.py
@@ -2,18 +2,14 @@ import socketio
 
 sio = socketio.Client()
 
-
-@sio.on("connect")
+@sio.on('connect')
 def connect():
-    print("Connected to server")
-    sio.emit("subscribe", {"room": "driver:1234"})
+  print('Connected to server')
+  sio.emit('subscribe', {'room': 'testing:testing'})
 
-
-@sio.on("coordinates")
+@sio.on('coordinates')
 def coordinates(data):
-    print("Received coordinates ", data)
+  print('Received coordinates ', data)
 
-
-# sio.connect('http://localhost:3000') # connect to server
-sio.connect("https://shuttle-up-tracking.herokuapp.com")  # connect to server
+sio.connect('http://localhost:3000') # connect to server
 sio.wait()
