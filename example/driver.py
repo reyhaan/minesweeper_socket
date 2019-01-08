66d36b4297772b74af60fbfe098aa215edad5176
Tk Yussuff <tktaofik@gmail.com>
2019-01-08 17:27:19 -0500

    Update driver.py

--- example/driver.py
+++ example/driver.py
@@ -2,12 +2,16 @@ import socketio
 
 sio = socketio.Client()
 
-@sio.on('connect')
-def connect():
-  print('Connected to server')
-  print('Sending coordinates')
-  for i in range(100):
-    sio.emit('coordinates', {'lat': 'teating', 'lon': 'testing', 'room': 'testing:testing'})
 
-sio.connect('http://localhost:3000') # connect to server
+@sio.on("connect")
+def connect():
+    print("Connected to server")
+    print("Sending coordinates")
+    for i in range(100):
+        sio.emit(
+            "coordinates", {"lat": 20.222, "lon": 34.444, "room": "testing:testing"}
+        )
+
+
+sio.connect("http://localhost:3000")  # connect to server
 sio.disconnect()
