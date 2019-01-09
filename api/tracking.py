6b1ae2c41c3e171eec396afe1a210d038140bb30
tktaofik <tktaofik@users.noreply.github.com>
2019-01-08 22:27:18 -0500

    testing hard coded redis url

--- api/tracking.py
+++ api/tracking.py
@@ -3,13 +3,20 @@ import os
 
 from typing import Dict
 
-redis_mgr = socketio.RedisManager(os.getenv('REDIS_URL') if os.getenv('ENV') is not 'dev' else os.getenv('REDIS_DEV'))
-sio = socketio.AsyncServer(async_mode='aiohttp')
+redis_mgr = socketio.RedisManager(
+    "redis://h:pc34db66b4af76a09bb1f3698a2ff340a76ed4f9ffdd7800f621c66780f8f9a34@ec2-3-81-188-41.compute-1.amazonaws.com:24679"
+    if os.getenv("ENV") is not "dev"
+    else os.getenv("REDIS_DEV")
+)
+sio = socketio.AsyncServer(async_mode="aiohttp")
 
-@sio.on('subscribe')
+
+@sio.on("subscribe")
 def subscribe(sid: str, data: Dict[str, str]):
-    sio.enter_room(sid, data['room'])
+    sio.enter_room(sid, data["room"])
 
-@sio.on('coordinates')
+
+@sio.on("coordinates")
 async def coordinates(sid, data: Dict[str, str]):
-  await sio.emit(event='coordinates', data=data, room=data['room'])
+    await sio.emit(event="coordinates", data=data, room=data["room"])
+
