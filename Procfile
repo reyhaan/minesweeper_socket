dc018761299d875808b5c86236ef76ff52e78d6e
tktaofik <tktaofik@users.noreply.github.com>
2019-01-08 22:30:18 -0500

    added procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web
+web: python app.py
--- api/tracking.py
+++ api/tracking.py
@@ -4,9 +4,7 @@ import os
 from typing import Dict
 
 redis_mgr = socketio.RedisManager(
-    "redis://h:pc34db66b4af76a09bb1f3698a2ff340a76ed4f9ffdd7800f621c66780f8f9a34@ec2-3-81-188-41.compute-1.amazonaws.com:24679"
-    if os.getenv("ENV") is not "dev"
-    else os.getenv("REDIS_DEV")
+    os.getenv("REDIS_URL") if os.getenv("ENV") is not "dev" else os.getenv("REDIS_DEV")
 )
 sio = socketio.AsyncServer(async_mode="aiohttp")
 
