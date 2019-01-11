d08955e6cb496fec46482abec222a71b098213ab
Lobarr <jesulobaegunjobi@hotmail.com>
2019-01-10 20:03:39 -0500

    completed status test

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,7 +10,7 @@ import dotenv
 import json
 import socketio
 from aiohttp import ClientSession
-from expects import expect, be, have_keys, have_key
+from expects import expect, equal, have_keys, have_key
 
 @pytest.fixture
 async def server(aiohttp_client):
@@ -18,35 +18,9 @@ async def server(aiohttp_client):
     server = await app.init()
     return await aiohttp_client(server)
 
-async def test_tracking_status(server):
+async def test_tracking_start(server):
     response = await server.get('/')
-    expect(response.status).to(be(200))
-    expect(await response.json()).to(have_key('Shuttle-up'))
-
-def test_tracking_service(server):
-    admin = socketio.Client()
-    driver = socketio.Client()
-
-    coordinates = {"lat": 20.222, "lon": 34.444, "room": "testing"}
-
-    def admin_connect():
-        admin.emit("subscribe", {"room": "testing"})
-
-    def admin_coordinates(data):
-        expect(data).to(have_keys("lat", "lon", "room"))        
-
-    def driver_connect():
-        driver.emit("coordinates", coordinates)
-
-    admin.on("connect", admin_connect)
-    admin.on("coordinates", admin_coordinates)
-    driver.on("connect", driver_connect)
-    
-    admin.connect(f"http://{server.host}:{str(server.port)}")
-    driver.connect(f"http://{server.host}:{str(server.port)}")
-    driver.disconnect()
-    admin.disconnect()
-
-
-
-
+    expect(response.status).to(equal(200))
+    data = await response.json()
+    expect(data).to(have_key("Shuttle-up"))
+    expect(data["Shuttle-up"]).to(equal("Event Service"))
